7584db3be92a10c82e55ac88fcd01b8f487a27e2
Kevin Watson <git-contact@watsonkp.com>
2023-06-12 12:09:16 -0400

    Added initial proxy library implementation

---
+++ proxy/src/lib.rs
@@ -0,0 +1,47 @@
+use std::time::SystemTime;
+use std::io::{Read, Write};
+use std::io::BufReader;
+use std::net::TcpListener;
+use std::thread;
+use std::sync::mpsc;
+
+use parse;
+
+pub fn start(address: &'static str, sender: mpsc::Sender<parse::Request>) -> thread::JoinHandle<()> {
+    thread::spawn(move || {
+        let listener = TcpListener::bind(address).unwrap();
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _addr)) => {
+                    let timestamp = match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+                        Ok(n) => n.as_millis(),
+                        Err(_) => 0,
+                    };
+                    // TODO: When does reading stop?
+                    // An HTTP request will keep the connection open while waiting for a response.
+                    // Detect the various end patterns "\r\n\r\n" etc.
+                    let mut buf_reader = BufReader::new(&stream);
+                    let mut body = Vec::new();
+                    let mut buf = [0u8; 1024];
+                    loop {
+                        match buf_reader.read(&mut buf) {
+                            Ok(n) => {
+                                body.extend(&buf);
+                                if n <= 1024 {
+                                    break;
+                                }
+                            },
+                            Err(_) => { },
+                        };
+                    }
+                    match sender.send(parse::Request::new(timestamp, body)) {
+                        Ok(_) => {},
+                        Err(_) => {},
+                    };
+                    stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
+                },
+                Err(_) => { },
+            };
+        }
+    })
+}
